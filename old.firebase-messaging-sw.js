6c979dd68ddb053a8df1c90e41748d96f711008a
Danilo Tomaccio <danilo.tomaccio@gmail.com>
2022-03-08 23:19:55 +0000

    Primo

---
+++ old.firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_5sSUzZkMofwuI4f6uF1CdKa5uBBzSU4",
+    authDomain: "pcgltricarico05.firebaseapp.com",
+    databaseURL: "https://pcgltricarico05-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "pcgltricarico05",
+    storageBucket: "pcgltricarico05.appspot.com",
+    messagingSenderId: "320152077505",
+    appId: "1:320152077505:web:f4c84686f061134a78c040",
+    measurementId: "G-7ZHL4J15XG"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(messaging, (payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
