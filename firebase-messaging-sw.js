8c5bc5387c9a2f45b71187b431096dbe5c983e47
Danilo Tomaccio <danilo.tomaccio@gmail.com>
2022-03-08 16:55:57 +0000

    Primo

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js')
 
-import { getMessaging } from "firebase/messaging";
-import { onBackgroundMessage } from "firebase/messaging/sw";
-
 const firebaseConfig = {
     apiKey: "AIzaSyC_5sSUzZkMofwuI4f6uF1CdKa5uBBzSU4",
     authDomain: "pcgltricarico05.firebaseapp.com",
@@ -12,22 +9,7 @@ const firebaseConfig = {
     storageBucket: "pcgltricarico05.appspot.com",
     messagingSenderId: "320152077505",
     appId: "1:320152077505:web:f4c84686f061134a78c040",
-    // measurementId: "G-7ZHL4J15XG"
+    measurementId: "G-7ZHL4J15XG"
 };
-initializeApp(firebaseConfig);
 
-const messaging = getMessaging();
-messaging.getToken({ vapidKey: "BOZDAwlyoFZqyXvJvmwtGxv3kYLHPRSdXa3V0va9tNN_2fjo5jk3FF6y8Muf2hJFtZlsypbCwmkL2W7NX9L4GkU" });
-
-onBackgroundMessage(messaging, (payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
-
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
+const app = firebase.initializeApp(firebaseConfig);
